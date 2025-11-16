f7958052cae19138dbcbdd4e33a5d0d75bc788bf
cyberdefenc <utkarshkushwahaofficial29@gmail.com>
2025-11-16 11:13:22 +0530

    if-else

--- Practice/Front_end_development/D10 React JS/firstproject/src/App.js
+++ Practice/Front_end_development/D10 React JS/firstproject/src/App.js
@@ -1,21 +1,46 @@
 // App.js
-import React from 'react'; // optional with new JSX transform but safe
-import { Container } from 'react-bootstrap';
+import React, { useState } from 'react';
+
+import { Button, Container } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from "./Header";
 import Footer from "./Footer";
 import "./App.css";
 
 function Card() {
+  let template = '';
+  let [count, setCount] = useState(1);
+  let [pshow, setPshow] = useState(false);
+
+  if (pshow) {
+    template = <>
+     <button onClick={()=>setPshow(!pshow)} >Hide</button>   <p>India</p></>
+
+  }
+  else {
+    template =  <button onClick={()=>setPshow(!pshow)} >Show</button>  
+  }
+
+  let displayData = () => {
+    setCount(count + 1);
+
+  }
   return (
-    <div className="carditems">
-      This is Card Component
-    </div>
+    <>
+      {template}
+      <Button className='bg-danger' onClick={displayData}>Increase n</Button>
+
+      <div className="carditems">
+        This is Card Component n = {count}
+      </div>
+    </>
   );
+
 }
 
 export default function App() {
   return (
+
     <div className="main">
       <Container className="my-3">
         <Header />
